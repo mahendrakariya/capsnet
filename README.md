614951974f3e1c9799058a8c88a095b82798d5ca
Mahendra Kariya <mahendra.kariya@go-jek.com>
2018-11-30 10:24:48 +0530

    Initial project setup using cookiecutter

---
+++ README.md
@@ -0,0 +1,5 @@
+capsnet
+==============================
+
+Implementation of the paper 'Dynamic Routing Between Capsules' (https://arxiv.org/pdf/1710.09829.pdf)
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementation of the paper 'Dynamic Routing Between Capsules' (https://arxiv.org/pdf/1710.09829.pdf)',
+    author='Mahendra K.',
+    license='',
+)
