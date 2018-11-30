b5b2cd3c7cfe1924d9f382531eefee08c8832dec
Mahendra Kariya <mahendra.kariya@go-jek.com>
2018-11-30 22:08:15 +0530

    Add mask layer

--- capsnet/layers/lambda_layers.py
+++ capsnet/layers/lambda_layers.py
@@ -1,4 +1,5 @@
 from keras import layers
+import tensorflow.keras.backend as K
 
 from capsnet.layers.activation import squash
 
@@ -9,3 +10,18 @@ def primary_capsule_layer(inputs, capsule_dim, n_channels, kernel_size=9, stride
     out = layers.Reshape(target_shape=[-1, capsule_dim])(out)
     return layers.Lambda(squash)(out)
 
+
+class Mask(layers.Layer):
+    def call(self, inputs, **kwargs):
+        if type(inputs) is list:
+            inputs, mask = inputs
+        else:
+            x = K.sqrt(K.sum(K.square(inputs), -1))
+            mask = K.one_hot(indices=K.argmax(x, 1), num_classes=x.get_shape().as_list()[1])
+        return K.batch_flatten(inputs * K.expand_dims(mask, -1))
+
+    def compute_output_shape(self, input_shape):
+        if type(input_shape[0]) is tuple:
+            return tuple([None, input_shape[0][1] * input_shape[0][2]])
+        else:
+            return tuple([None, input_shape[1] * input_shape[2]])
